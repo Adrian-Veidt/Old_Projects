8037820292ce4adaf4a363b7e944ee53d01929d8
Andrii Vasin <andriivasin@gmail.com>
2017-06-01 11:26:58 +0300

    I dont get it! Sometimes cyrillics just work. Other times - no. Encoding the same. Hmm....

--- Cheater/Cheater/Game.cpp
+++ Cheater/Cheater/Game.cpp
@@ -1,6 +1,6 @@
 /*Cheater
-Author: Андрей Васин andriivasin@gmail.com
-Программа для Шага, "угадывает" число.
+Author: Andrii Vasin andriivasin@gmail.com
+Project for STEP academy. 
 */
 
 #include <iostream>
@@ -9,13 +9,13 @@ using namespace std;
 void main()
 
 {
-	cout << "Let`s play!\n"; //Приглашение поиграть
+	cout << "Let`s play!\n"; //Invitation to play
 
-	int i = 0; //Объявление переменной, я сразу дал ей значение ноль
+	int i = 0; //Declaration of variable
 
 	cout << "Please, enter the number. Those who have bigger - wins:\t";
 
-	cin >> i; //ВВод числа
+	cin >> i; //Input number
 	
 	cout << "\nI have: " << i + 1 << "\n";
 	cout << "I`m winner!\n\n";
