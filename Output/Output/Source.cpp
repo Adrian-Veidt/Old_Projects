eb84a61da02e345d8caa92ca916b8bc01f8cb100
Andrii Vasin <andriivasin@gmail.com>
2017-06-01 08:15:56 +0300

    basic output

---
+++ Output/Output/Source.cpp
@@ -0,0 +1,13 @@
+/* Проект Output
+Author: Андрей Васин andriivasin@gmail.com
+вторая программа для ШАГа, вывод типов данных
+*/
+#include <iostream>
+
+using namespace std;
+
+void main()
+
+{
+	cout << "\t\t\t\"Congratulation with new beginning!\"\n\n\n";
+}
